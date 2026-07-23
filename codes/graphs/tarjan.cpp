af5a15aec2cd6a2d82fb746ba934a2fc10576134
GuilhermeKK23 <gui.kavakami@gmail.com>
2026-07-23 10:59:29 -0300

    Shorter code

--- codes/graphs/tarjan.cpp
+++ codes/graphs/tarjan.cpp
@@ -4,23 +4,18 @@ using namespace std;
 const int INF = 1e9+10;
 
 void dfs(int cur, int &t, vector<vector<int>> &grafo, vector<bool> &marc, vector<bool> &lead, vector<int> &low, vector<int> &comp, stack<int> &s){
-    marc[cur] = true;
+    marc[cur] = lead[cur] = true;
     low[cur] = t++;
-    lead[cur] = true;
     for(int viz:grafo[cur]){
         if(!marc[viz]) dfs(viz, t, grafo, marc, lead, low, comp, s);
         if(low[viz]<low[cur]) low[cur] = low[viz], lead[cur] = false;
     }
+    s.push(cur);
     if(lead[cur]){
         while(s.size() && low[s.top()]>=low[cur]){
             int x = s.top(); s.pop();
-            comp[x] = cur;
-            low[x] = INF;
+            comp[x] = cur, low[x] = INF;
         }
-        comp[cur] = cur;
-        low[cur] = INF;
-    }else{
-        s.push(cur);
     }
 }
 
